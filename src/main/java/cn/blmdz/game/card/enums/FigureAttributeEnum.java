873fcaeea851b6dcb717f3cb853e244283323ac4
xpoll <blmdz521@126.com>
2022-10-26 15:47:29 +0800

    1

--- src/main/java/cn/blmdz/game/card/enums/FigureAttributeEnum.java
+++ src/main/java/cn/blmdz/game/card/enums/FigureAttributeEnum.java
@@ -1,15 +1,22 @@
 package cn.blmdz.game.card.enums;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * 基础属性
  */
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public enum FigureAttributeEnum {
-    ATTACK, // 攻击
-    DEFENSE, // 防御
-    BLOOD, // 气血
-    SPEED, // 速度
-    DODGE, // 闪避
-    PARRY, // 招架
-    HIT, // 命中
+    ATTACK("攻击"),
+    DEFENSE("防御"),
+    BLOOD("气血"),
+    SPEED("速度"),
+    DODGE("闪避"),
+    PARRY("招架"),
+    HIT("命中"),
     ;
+    private final String description;
 }
